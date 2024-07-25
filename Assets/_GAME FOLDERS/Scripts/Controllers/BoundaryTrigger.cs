e2014a39c09a53ae391b669c1a42b0340ffb9f65
Burak ÖZGEN <35861357+Burakzgen@users.noreply.github.com>
2024-07-26 00:09:54 +0300

    add boundary region

---
+++ Assets/_GAME FOLDERS/Scripts/Controllers/BoundaryTrigger.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class BoundaryTrigger : MonoBehaviour
+{
+    [SerializeField] CanvasGroup boundaryWarningImage;
+    IHealth health;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            health = other.GetComponent<PlayerHealth>();
+            boundaryWarningImage.DOFade(1, 0.25f);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            health.TakeDamage(1);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            boundaryWarningImage.DOFade(0, 0.25f);
+        }
+    }
+}
